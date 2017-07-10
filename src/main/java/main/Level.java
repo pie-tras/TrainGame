15a565228ccbc0e20c31d17e70bbbd166ed1a07e
Brad <brad@morsecode-inc.com>
2017-07-09 21:06:00 -0600

    roughed out a Level object.

---
+++ src/main/java/main/Level.java
@@ -0,0 +1,43 @@
+package main;
+
+
+/**
+ * Represents a level in the game
+ * @author morsecode
+ *
+ */
+public class Level implements Comparable<Level> {
+    
+    private int value;
+    
+    public Level() {
+        this.value= 1;  // the first level
+    }
+    
+    private Level(int value) {
+        this.value= Math.abs(value);    // don't allow levels < 0
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
+    /**
+     * Returns <=-1 if this < otherLevel
+     * Returns 0 if this == otherLevel
+     * Returns >=1  if this > otherLevel 
+     */
+    @Override
+    public int compareTo(Level otherLevel) {
+        if (otherLevel == null) {
+            return 1;   // something > null
+        }
+        
+        // simple way to return < 0, 0, or > 0 with some math
+        return value - otherLevel.value;
+    }
+    
+    public Level getNextLevel() {
+        return new Level(value + 1);
+    }
+}
